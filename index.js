a8e4df6824352b8b80af6552fe4e2c00c8349af5
Aaron Smith <aaron.j.smith2019@gmail.com>
2022-11-05 15:48:38 -0700

    Update module to use promises

--- index.js
+++ index.js
@@ -1,16 +1,13 @@
-const fs = require('fs');
+const fs = require('fs/promises');
 const { csvToJSON } = require('./csvToJSON.js');
 
-module.exports = (csvFilePath, newJSONFilePath, convertBooleanAndNullStrings = false) => {
-  const csvData = fs.readFileSync(csvFilePath, { encoding: 'utf8' });
-  const JSONData = csvToJSON(csvData, convertBooleanAndNullStrings);
-
-  fs.writeFile(newJSONFilePath, JSON.stringify(JSONData), (err) => {
-      if (err) {
-          console.log(err);
-      } else {
-          console.log('CSV to JSON Conversion complete!');
-      }
-  });
+module.exports = async (csvFilePath = './test.csv', newJSONFilePath = './test.json', convertBooleanAndNullStrings = false) => {
+  try {  
+    const csvData = await fs.readFile(csvFilePath, { encoding: 'utf8' });
+    const JSONData = csvToJSON(csvData, convertBooleanAndNullStrings);
+    await fs.writeFile(newJSONFilePath, JSON.stringify(JSONData));
+    console.log('CSV to JSON Conversion complete!');
+  } catch (err) {
+      console.log(err);
+  }
 };
-
