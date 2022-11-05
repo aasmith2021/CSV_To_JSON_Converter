db0ec3a561a5273bfa281eb3dc15eeeaaed7b128
Aaron Smith <aaron.j.smith2019@gmail.com>
2022-11-05 14:59:41 -0700

    Add convertBooleanAndNullStrings flag

--- csvToJSON.js
+++ csvToJSON.js
@@ -1,16 +1,34 @@
-const splitCSVLineToArray = (dataRow) => {
+const convertStringToBooleanOrNull = (textString) => {
+  switch (textString.trim().toLowerCase()) {
+    case 'true':
+      return true;
+    case 'false':
+      return false;
+    case 'null':
+      return null;
+    default:
+      return textString;
+  }
+};
+
+const splitCSVLineToArray = (dataRow, convertBooleanAndNullStrings) => {
     const resultArray = [];
 
     while (dataRow.length) {
         if (dataRow.indexOf('"') === 0) {
             const indexOfClosingQuotationMark = dataRow.substring(1).indexOf('"') + 1;
-            resultArray.push(dataRow.substring(1, indexOfClosingQuotationMark));
+            const text = dataRow.substring(1, indexOfClosingQuotationMark);
+            const result = convertBooleanAndNullStrings ? convertStringToBooleanOrNull(text) : text;
+            resultArray.push(result);
             dataRow = dataRow.substring(indexOfClosingQuotationMark + 2);
         } else if (dataRow.indexOf(',') !== -1) {
-            resultArray.push(dataRow.substring(0, dataRow.indexOf(',')));
+            const text = dataRow.substring(0, dataRow.indexOf(','));
+            const result = convertBooleanAndNullStrings ? convertStringToBooleanOrNull(text) : text;
+            resultArray.push(result);
             dataRow = dataRow.substring(dataRow.indexOf(',') + 1);
         } else if (dataRow.indexOf(',') === -1 && dataRow.length !== 0) {
-            resultArray.push(dataRow);
+            const result = convertBooleanAndNullStrings ? convertStringToBooleanOrNull(dataRow) : dataRow;
+            resultArray.push(result);
             dataRow = '';
         }
     }
@@ -22,17 +40,19 @@ const splitCSVLineToArray = (dataRow) => {
  * Converts raw CSV data into an array of JSON objects. Each object created sets its property keys
  * from the first row of CSV data, and each property value is set corresponding row data.
  * @param {String} rawCSVData - A single string with the CSV data
+ * @param {Boolean} convertBooleanAndNullStrings - A flag indicating if 'true', 'false', and 'null'
+ * strings should be converted to boolean and null values
  * @returns {[Object]} - An array of data objects populated with the key-value pairs
  * of the header data and the corresponding row data.
  */
-const csvToJSON = (rawCSVData) => {
+const csvToJSON = (rawCSVData, convertBooleanAndNullStrings = false) => {
     const lineBreakCharacter = rawCSVData.includes('\r\n') ? '\r\n' : '\n';
 
     [headerRow, ...dataRows] = rawCSVData.split(lineBreakCharacter);
     const headers = splitCSVLineToArray(headerRow);
 
     const allDataAsJSON = dataRows.map((dataRow) => {
-        const dataRowSplitToArray = splitCSVLineToArray(dataRow);
+        const dataRowSplitToArray = splitCSVLineToArray(dataRow, convertBooleanAndNullStrings);
         const dataObject = {};
 
         indexOfDataToAdd = 0;
--- index.js
+++ index.js
@@ -6,17 +6,13 @@ const { csvToJSON } = require('./csvToJSON.js');
 const filePathOfCSVFile = path.resolve(__dirname, './test.csv');
 const filePathOfNewJSONFile = path.resolve(__dirname, './test.json');
 
-const startTime = performance.now();
 const csvData = fs.readFileSync(filePathOfCSVFile, { encoding: 'utf8' });
-
-const JSONData = csvToJSON(csvData);
+const JSONData = csvToJSON(csvData, true);
 
 fs.writeFile(filePathOfNewJSONFile, JSON.stringify(JSONData), (err) => {
     if (err) {
         console.log(err);
     } else {
-        const endTime = performance.now();
         console.log('CSV to JSON Conversion complete!');
-        console.log('Conversion completed in:', endTime - startTime,'ms');
     }
 });
