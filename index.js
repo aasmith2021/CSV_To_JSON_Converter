8990ad8812db6e4c130126505d4d87cdfc76399c
Aaron Smith <aaron.j.smith2019@gmail.com>
2022-11-05 15:28:04 -0700

    Update module to accept absolute file paths

--- index.js
+++ index.js
@@ -1,15 +1,11 @@
 const fs = require('fs');
-const path = require('path');
 const { csvToJSON } = require('./csvToJSON.js');
 
-module.exports = (csvFilePath = './test.csv', newJSONFilePath = './test.json', convertBooleanAndNullStrings = false) => {
-  const filePathOfCSVFile = path.resolve(__dirname, csvFilePath);
-  const filePathOfNewJSONFile = path.resolve(__dirname, newJSONFilePath);
-
-  const csvData = fs.readFileSync(filePathOfCSVFile, { encoding: 'utf8' });
+module.exports = (csvFilePath, newJSONFilePath, convertBooleanAndNullStrings = false) => {
+  const csvData = fs.readFileSync(csvFilePath, { encoding: 'utf8' });
   const JSONData = csvToJSON(csvData, convertBooleanAndNullStrings);
 
-  fs.writeFile(filePathOfNewJSONFile, JSON.stringify(JSONData), (err) => {
+  fs.writeFile(newJSONFilePath, JSON.stringify(JSONData), (err) => {
       if (err) {
           console.log(err);
       } else {
